bf8f4a94f7536cd79be4a938e102bc8b2d5630d2
otson <59139705+otson@users.noreply.github.com>
2021-10-13 21:17:17 +0300

    add support for fen notation

--- src/app/chess-board/chess-board.component.ts
+++ src/app/chess-board/chess-board.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Piece} from "../piece";
+import {isDigit} from "@angular/compiler/src/chars";
 
 
 @Component({
@@ -14,10 +15,37 @@ export class ChessBoardComponent implements OnInit {
 
   @Input() board: number[] = new Array(64).fill(0);
 
+  private fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
+  private pieceFromSymbol = new Map([
+    ['k',Piece.King],
+    ['p',Piece.Pawn],
+    ['n',Piece.Knight],
+    ['b',Piece.Bishop],
+    ['r',Piece.Rook],
+    ['q',Piece.Queen],
+  ])
+
   ngOnInit(): void {
-    this.board[5] = Piece.King * Piece.Black;
-    this.board[6] = Piece.King * Piece.White;
-    this.board[23] = Piece.Rook * Piece.Black;
-    this.board[45] = Piece.Queen * Piece.White;
+    function isCharNumber(c: string) {
+      return c >= '0' && c <= '9';
+    }
+    let rank = 7;
+    let file = 0;
+    for(let i = 0; i < this.fen.length; i++){
+      let c = this.fen.charAt(i);
+      if(c == '/'){
+        file = 0;
+        rank--;
+      } else {
+        if(isCharNumber(c)){
+          file += Number.parseInt(c);
+        } else {
+          let color = (c === c.toUpperCase()) ? Piece.Black : Piece.White;
+          let piece = this.pieceFromSymbol.get(c.toLowerCase())!;
+          this.board[rank * 8 + file] = color * piece;
+          file++;
+        }
+      }
+    }
   }
 }
